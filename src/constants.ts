7567f05065e7d332dbd48351a6dcb71759da21b9
fanghaoming <fanghaoming@rayvision.com>
2025-11-06 15:22:31 +0800

    :sparkles: Update BASE_URL to use dynamic protocol and hostname; reset STORAGE_KEYS token; increase DEFAULT_CONCURRENCY to 5

--- src/constants.ts
+++ src/constants.ts
@@ -1,12 +1,12 @@
 export const BASE_URL = (typeof window !== 'undefined' && /localhost|127\.0\.0\.1/.test(location.hostname))
   ? '/ztapi'
-  : 'http://www.zentao.rayvision.com/zentao/api.php/v1'
+  : `${location.protocol}//${location.hostname}/zentao/api.php/v1`
 
 export const STORAGE_KEYS = {
-  token: 'd92a242cb8ee23303a5cd5dbb9570e3b',
+  token: '',
   filters: 'zentao_filters'
 } as const
 
-export const DEFAULT_CONCURRENCY = 3
+export const DEFAULT_CONCURRENCY = 5
 
 
